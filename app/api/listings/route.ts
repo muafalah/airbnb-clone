27a8e4adcb49a9ab4a62bb11e983718ca3d0a859
muafalah <muh.ifa.amrillah@gmail.com>
2023-09-09 21:56:32 +0700

    feat: add Listing API

---
+++ app/api/listings/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/libs/prismadb";
+import getCurrentUser from "@/actions/getCurrentUser";
+
+export async function POST(req: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const body = await req.json();
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+  } = body;
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing);
+}
