8b30a654b8d79efa8513567c2a7abb0585ec104c
muafalah <muh.ifa.amrillah@gmail.com>
2023-09-10 20:10:52 +0700

    fix bug ssr

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,11 @@ const Home = async ({ searchParams }: HomeProps) => {
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {listings?.map((listing: any) => (
+        {listings.map((listing: any) => (
           <ListingCard
+            currentUser={currentUser}
             key={listing.id}
             data={listing}
-            currentUser={currentUser}
           />
         ))}
       </div>
